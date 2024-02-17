cd7bdf25b3a87a75fed05634dbf6e4d74c1636b7
vaneath <vaneathzz@gmail.com>
2024-02-17 11:33:59 +0700

    close #1 add user, department, position migration and its model and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,38 +2,38 @@
 
 namespace Database\Factories;
 
+use App\Models\Department;
+use App\Models\Position;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use RoleEnum;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+  /**
+   * The current password being used by the factory.
+   */
+  protected static ?string $password;
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'user_id' => random_int(100, 999),
+      'first_name' => $this->faker->firstName,
+      'last_name' => $this->faker->lastName,
+      'role' => random_int(1, 3),
+      'department_id' => Department::all()->random()->id,
+      'position_id' => Position::all()->random()->id,
+      'password' => Hash::make('password'),
+    ];
+  }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,31 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Department;
+use App\Models\Position;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    Department::factory()->count(5)->create();
+    Position::factory()->count(10)->create();
+    User::factory()->count(30)->create();
+    User::factory()->create([
+      'user_id' => 123,
+      'first_name' => 'V',
+      'last_name' => 'A',
+      'role' => 1,
+      'department_id' => Department::all()->random()->id,
+      'position_id' => Position::all()->random()->id,
+      'password' => 'password',
+    ]);
+  }
 }
