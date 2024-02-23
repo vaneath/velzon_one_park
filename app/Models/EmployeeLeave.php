ea4e2a38d6ccf015024411eeca8d60268f83937c
vaneath <vaneathzz@gmail.com>
2024-02-23 22:48:59 +0700

    close #18 add fillable to employee leave model and factory

--- app/Models/EmployeeLeave.php
+++ app/Models/EmployeeLeave.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeLeave extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'annual_leave',
+        'annual_leave_half',
+        'accidental_leave',
+        'sick_leave',
+        'special_leave',
+        'unpaid_leave',
+        'maternity_leave',
+        'compenstate_leave',
+    ];
 }
